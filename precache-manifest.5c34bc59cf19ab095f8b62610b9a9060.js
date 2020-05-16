e812be41042a6ca8e03f23ab1e231e2f74439d73
russo-anthony <russoa15@tcnj.com>
2020-05-16 17:21:41 -0400

    Updates

---
+++ precache-manifest.5c34bc59cf19ab095f8b62610b9a9060.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9691aaf8bd496ec1c4ede603949e8fe",
+    "url": "/russo-portfolio/index.html"
+  },
+  {
+    "revision": "f107212255afc1edad57",
+    "url": "/russo-portfolio/static/css/main.30cc9a30.chunk.css"
+  },
+  {
+    "revision": "8e85991b749b8ef7fa7a",
+    "url": "/russo-portfolio/static/js/2.1532bcb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/russo-portfolio/static/js/2.1532bcb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f107212255afc1edad57",
+    "url": "/russo-portfolio/static/js/main.761ca335.chunk.js"
+  },
+  {
+    "revision": "e1a60056a930e2961e49",
+    "url": "/russo-portfolio/static/js/runtime-main.1fd127ec.js"
+  },
+  {
+    "revision": "0ad2704c857990df30e1a467e08d6201",
+    "url": "/russo-portfolio/static/media/Dayum.0ad2704c.png"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/russo-portfolio/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/russo-portfolio/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "d1f95353a09c7afd544e9ae2bc8f5af7",
+    "url": "/russo-portfolio/static/media/fa-brands-400.d1f95353.svg"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/russo-portfolio/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/russo-portfolio/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/russo-portfolio/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "6c6a5fad8ed5136b92d0ead30b2328d3",
+    "url": "/russo-portfolio/static/media/fa-regular-400.6c6a5fad.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/russo-portfolio/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/russo-portfolio/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/russo-portfolio/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "0ffddc07ec1115efbe1833a0023a711e",
+    "url": "/russo-portfolio/static/media/fa-solid-900.0ffddc07.svg"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/russo-portfolio/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/russo-portfolio/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/russo-portfolio/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/russo-portfolio/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "b0e594144f6569f9a9d4683ebb9f2da7",
+    "url": "/russo-portfolio/static/media/overlay.b0e59414.png"
+  },
+  {
+    "revision": "94d7c6f7aff9444ba970bccb9276b68d",
+    "url": "/russo-portfolio/static/media/pic02.94d7c6f7.jpg"
+  },
+  {
+    "revision": "4ad678e16d88aa7c5155c6340db88064",
+    "url": "/russo-portfolio/static/media/pic03.4ad678e1.jpg"
+  },
+  {
+    "revision": "0f978ead34f7c7c78fa43a326dc38237",
+    "url": "/russo-portfolio/static/media/pic04.0f978ead.jpg"
+  },
+  {
+    "revision": "d585f3cebb7d5da74c4ad1c275d594b4",
+    "url": "/russo-portfolio/static/media/pic07.d585f3ce.jpg"
+  },
+  {
+    "revision": "ccecd27dc33732137a0b5200f616e4a2",
+    "url": "/russo-portfolio/static/media/pic08.ccecd27d.jpg"
+  }
+]);
